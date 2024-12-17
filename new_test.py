6f02e05e87d96abb1e1a116c5ed91781725cf37a
aminnaz93 <Aminnazari040@hotmail.com>
2024-12-17 10:07:13 +0100

    test file commit

---
+++ new_test.py
@@ -0,0 +1,47 @@
+from main import *
+from cryptography import fernet
+import json
+
+def test_generate_key():
+    password = "pippi"
+
+    key = generate_key(password)
+
+    assert isinstance(key, bytes)
+    assert len(key) == 44
+
+##############################################################################################
+
+def test_encrypt():
+    password = "pippi"
+    key = generate_key(password)
+    fernet = Fernet(key)
+
+    message = {"username": "testing_user", "message": "Heeeej på daaaaaj"}
+
+    encrypt_this_message = encrypt(message, fernet)
+
+    assert isinstance(encrypt_this_message, bytes)
+    assert encrypt_this_message != json.dumps(message).encode()
+
+##############################################################################################
+
+def test_decrypt():
+    password = "pippi"
+    key = generate_key(password)
+    fernet = Fernet(key)
+
+    message = {"username": "testing_user", "message": "Heeeej på daaaaaj" }
+
+    encrypt_this_message = encrypt(message,fernet)
+
+    decrypt_this_message = decrypt(encrypt_this_message, fernet)
+
+    assert decrypt_this_message == message
+
+##############################################################################################
+
+
+
+
+
